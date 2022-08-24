ba4b7c777226d36de5eb864e44921619a591d174
Ishaan mandhar <90463540+Mishaan@users.noreply.github.com>
2022-08-24 14:16:11 +0530

    Update server.js

--- server.js
+++ server.js
@@ -2,15 +2,9 @@ require('dotenv').config();
 const express = require('express');
 const app = express();
 const path = require('path');
+
 // this is for running the server ...
 const PORT = process.env.PORT || 3000;
-
-const cors = require('cors');
-// Cors 
-const corsOptions = {
-  origin: process.env.ALLOWED_CLIENTS.split(',')
-  // ['http://localhost:3000', 'http://localhost:5000', 'http://localhost:3300']
-}
 
 app.use(express.static('public'));
 app.use(express.static(__dirname));
